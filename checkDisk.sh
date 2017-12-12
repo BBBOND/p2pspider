c1d9c12656ae4735156759137d911904846d63fb
BBBOND <jwy8645@gmail.com>
2017-12-12 10:44:38 -0500

    shell

--- checkDisk.sh
+++ checkDisk.sh
@@ -13,13 +13,10 @@ echo ${disk}
 # --------------------------------
 #  Start to check the disk space
 # --------------------------------
-while [ disk ]
-    do
-        percent=`df -P ${disk} | tail -1 | awk '{print $5 }' | cut -d'%' -f1`
-        if [ "${percent}" -ge "${max}" ]; then
-            echo '磁盘告警'
-            pm2 stop all
-	    break
-        fi;
-        shift
-    done;
+percent=`df -P ${disk} | tail -1 | awk '{print $5 }' | cut -d'%' -f1`
+if [ "${percent}" -ge "${max}" ]; then
+	echo '磁盘告警'
+        pm2 stop all;
+else
+	echo 磁盘占用 ${percent};
+fi;
--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-/home/p2pspider/p2pspider/checkDisk.sh 90 > log 2>&1
-pm2 start /home/p2pspider/p2pspider/index.js -i 4
+pm2 start /home/p2pspider/p2pspider/index.js -i 4 >log 2>&1
+
+#/home/p2pspider/p2pspider/checkDisk.sh 90 >>log 2>&1;
