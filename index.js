b84679d06d7b8bc912d34d49191e2bea841af464
BBBOND <jwy8645@163.com>
2017-12-21 01:24:54 +0800

    不保存种子文件，直接解析后保存数据库，之后直接删除文件

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 'use strict';
-
-// This is an example of using p2pspider, you can change the code to make it do something else.
 let fs = require('fs');
 let path = require('path');
 let bencode = require('bencode');
 let P2PSpider = require('./lib');
-let moment = require('moment');
+let MongoUtil = require('./src/utils/MongoUtil');
+let parseTorrentFile = require('./src/utils/TorrentParseUtil');
+let Torrent = require('./src/model/Torrent');
+let logger = require('pomelo-logger').getLogger('index.js');
 
 const p2p = P2PSpider({
     nodesMaxSize: 400,
     maxConnections: 800,
     timeout: 10000
 });
+
+const getTorrent = (torrentPath) => {
+    let torrent = parseTorrentFile(torrentPath);
+    torrent._id = torrent.infoHash;
+    return torrent;
+};
 
 p2p.ignore((infohash, rinfo, callback) => {
     let torrentFilePathSaveTo = path.join(__dirname, "torrents", infohash + ".torrent");
@@ -22,14 +29,34 @@ p2p.ignore((infohash, rinfo, callback) => {
 
 p2p.on('metadata', (metadata) => {
     let torrentFilePathSaveTo = path.join(__dirname, "torrents", metadata.infohash + ".torrent");
-    fs.writeFile(torrentFilePathSaveTo, bencode.encode({'info': metadata.info}), (err) => {
+    fs.writeFile(torrentFilePathSaveTo, bencode.encode({'info': metadata.info}), async (err) => {
         if (err) {
-            return console.error(err);
+            return logger.error(err);
         }
-        let torrentFileListPath = path.join(__dirname, "torrentsList", moment().format('YYYY-MM-DD') + ".txt");
-        fs.appendFileSync(torrentFileListPath, metadata.infohash + ".torrent\n");
-        // console.log(metadata.infohash + ".torrent has saved.");
+        let torrent;
+        try {
+            torrent = getTorrent(torrentFilePathSaveTo, true);
+        } catch (e) {
+            fs.unlinkSync(torrentFilePathSaveTo);
+            return logger.error(e);
+        }
+        if (!!torrent) {
+            try {
+                let result = await MongoUtil.insertDocUnique(Torrent, torrent);
+                if (result && result.msg) {
+                    logger.info(result.msg);
+                } else {
+                    logger.info(`${metadata.infohash} => ${!!result ? '保存成功' : '保存失败'}`);
+                }
+                return fs.unlinkSync(torrentFilePathSaveTo);
+            } catch (e) {
+                MongoUtil.connect();
+                return fs.unlinkSync(torrentFilePathSaveTo);
+            }
+        }
+        return null;
     });
 });
 
+MongoUtil.connect();
 p2p.listen(6881, '0.0.0.0');
